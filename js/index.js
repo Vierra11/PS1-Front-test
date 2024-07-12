02c46ee5e49e2718ef461497703caf5c9f82e1a4
Vierra11 <vrishti.godhwani@gmail.com>
2024-07-12 13:04:41 +0530

    minor change

--- js/index.js
+++ js/index.js
@@ -1,25 +1,27 @@
-function getQueryParams() {
-    const params = {};
-    window.location.search
-        .substr(1)
-        .split("&")
-        .forEach(function(item) {
-        const [key, value] = item.split("=");
-        if (key) params[key] = decodeURIComponent(value);
-    });
-    return params;
-}
+$(document).ready(function() {
+    function getQueryParams() {
+        const params = {};
+        window.location.search
+            .substr(1)
+            .split("&")
+            .forEach(function(item) {
+            const [key, value] = item.split("=");
+            if (key) params[key] = decodeURIComponent(value);
+        });
+        return params;
+    }
 
-const queryElements = getQueryParams();
+    const queryElements = getQueryParams();
 
-// Change the date and time span
-const dateTimeSpan = document.getElementById("date_time");
-dateTimeSpan.textContent = `${queryElements.date} , ${queryElements.time}`;
+    // Change the date and time span
+    const dateTimeSpan = document.getElementById("date_time");
+    dateTimeSpan.textContent = `${queryElements.date} , ${queryElements.time}`;
 
-// Change the movie details
-const movieSpan = document.getElementById("movie_name");
-movieSpan.textContent = `${queryElements.movieName}`;
+    // Change the movie details
+    const movieSpan = document.getElementById("movie_name");
+    movieSpan.textContent = `${queryElements.movieName}`;
 
-// Change the theatre details
-const movieSpan = document.getElementById("theatre_name");
-movieSpan.textContent = `${queryElements.theatreName}`;
+    // Change the theatre details
+    const theatreSpan = document.getElementById("theatre_name");
+    theatreSpan.textContent = `${queryElements.theatreName}`;
+});
