c1f9f3b43da68fe58360715432b42e0c638d7c15
Vierra11 <vrishti.godhwani@gmail.com>
2024-07-12 12:32:51 +0530

    initial commit

---
+++ js/index.js
@@ -0,0 +1,25 @@
+function getQueryParams() {
+    const params = {};
+    window.location.search
+        .substr(1)
+        .split("&")
+        .forEach(function(item) {
+        const [key, value] = item.split("=");
+        if (key) params[key] = decodeURIComponent(value);
+    });
+    return params;
+}
+
+const queryElements = getQueryParams();
+
+// Change the date and time span
+const dateTimeSpan = document.getElementById("date_time");
+dateTimeSpan.textContent = `${queryElements.date} , ${queryElements.time}`;
+
+// Change the movie details
+const movieSpan = document.getElementById("movie_name");
+movieSpan.textContent = `${queryElements.movieName}`;
+
+// Change the theatre details
+const movieSpan = document.getElementById("theatre_name");
+movieSpan.textContent = `${queryElements.theatreName}`;
