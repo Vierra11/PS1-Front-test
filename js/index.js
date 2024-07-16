0deda1e65a72c74432a2c01b85b4bbe8353d4069
Vierra11 <vrishti.godhwani@gmail.com>
2024-07-16 09:44:25 +0530

    testing

--- js/index.js
+++ js/index.js
@@ -49,6 +49,13 @@ async function fetchData(uuid) {
       // Change the theatre details
       const theatreSpan = document.getElementById("theatre_name");
       theatreSpan.textContent = `${queryElements.theatreName}`;
+
+      // Set price and sold seats
+      window.price = parseInt(queryElements.price, 10);
+      const soldSeats = queryElements.soldSeats.split(',').map(seat => seat.trim());
+      const sc = $('#seat-map').seatCharts();
+      sc.get(soldSeats).status('unavailable');
+    
     }
   } catch (error) {
     console.error("There was an error: ", error);
@@ -57,64 +64,64 @@ async function fetchData(uuid) {
 
 fetchData(uuid);
 
-// Obtain selected_seats
-function getSelectedSeats() {
-  const selectedSeats = [];
-  document.querySelectorAll('#selected-seats li').forEach(seat => {
-    selectedSeats.push(seat.textContent.trim());
-  });
-  return selectedSeats.join(',');
-}
+// // Obtain selected_seats
+// function getSelectedSeats() {
+//   const selectedSeats = [];
+//   document.querySelectorAll('#selected-seats li').forEach(seat => {
+//     selectedSeats.push(seat.textContent.trim());
+//   });
+//   return selectedSeats.join(',');
+// }
 
-// Obtain total amount
-function getTotalAmount() {
-  return document.getElementById('total').textContent;
-}
+// // Obtain total amount
+// function getTotalAmount() {
+//   return document.getElementById('total').textContent;
+// }
 
-document.addEventListener("DOMContentLoaded", function () {
-  const redirectElement = document.getElementById("redirect-html");
-  const errorElement = document.getElementById("error-msg");
+// document.addEventListener("DOMContentLoaded", function () {
+//   const redirectElement = document.getElementById("redirect-html");
+//   const errorElement = document.getElementById("error-msg");
 
-  redirectElement.addEventListener("click", async (event) => {
-    const selectedSeats = getSelectedSeats();
-    const amount = getTotalAmount();
+//   redirectElement.addEventListener("click", async (event) => {
+//     const selectedSeats = getSelectedSeats();
+//     const amount = getTotalAmount();
 
-    try {
-      await sendData(uuid, amount, selectedSeats);
-      let paramStr = `?uuid=${encodeURIComponent(uuid)}&amount=${encodeURIComponent(amount)}&selected_seats=${encodeURIComponent(selectedSeats)}`;
-      redirectElement.href += paramStr;
-    } catch (error) {
-      console.error("Error sending data: ", error);
-      event.preventDefault();
-      errorElement.classList.add("active");
-      errorElement.textContent = "There was an error processing your request. Please try again.";
-    }
-  });
-});
+//     try {
+//       await sendData(uuid, amount, selectedSeats);
+//       let paramStr = `?uuid=${encodeURIComponent(uuid)}&amount=${encodeURIComponent(amount)}&selected_seats=${encodeURIComponent(selectedSeats)}`;
+//       redirectElement.href += paramStr;
+//     } catch (error) {
+//       console.error("Error sending data: ", error);
+//       event.preventDefault();
+//       errorElement.classList.add("active");
+//       errorElement.textContent = "There was an error processing your request. Please try again.";
+//     }
+//   });
+// });
 
-async function sendData(uuid, amount, selectedSeats) {
-  try {
-    const response = await fetch(WEBHOOK, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        "isMessageFromAWebApp": true,
-        "stage": "final",
-        "uuid": uuid,
-        "append": {
-        "amount": amount,
-        "selected_seats": selectedSeats
-        }
-      })
-    });
+// async function sendData(uuid, amount, selectedSeats) {
+//   try {
+//     const response = await fetch(WEBHOOK, {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json"
+//       },
+//       body: JSON.stringify({
+//         "isMessageFromAWebApp": true,
+//         "stage": "final",
+//         "uuid": uuid,
+//         "append": {
+//         "amount": amount,
+//         "selected_seats": selectedSeats
+//         }
+//       })
+//     });
 
-    if (!response.ok) {
-      throw new Error("Network response was not ok " + response.statusText);
-    } 
-  } 
-  catch (error) {
-  console.error("There was an error: ", error);
-  }
-}
+//     if (!response.ok) {
+//       throw new Error("Network response was not ok " + response.statusText);
+//     } 
+//   } 
+//   catch (error) {
+//   console.error("There was an error: ", error);
+//   }
+// }
